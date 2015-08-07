c9ee77c5939ec15c493bb66d4fd030fe322284a6
Tijndagamer <tijndagamer25@gmail.com>
2015-08-07 21:39:54 +0200

    Constants and method names are now PEP 8 compliant.

--- MPU6050.py
+++ MPU6050.py
@@ -8,30 +8,31 @@ import smbus
 class MPU6050:
 
     # Global Variables
-    gravityMS2 = 9.80665
+    GRAVITIY_MS2 = 9.80665
     address = None
     bus = smbus.SMBus(1)
 
     # Scale Modifiers
-    accelScaleModifier2G = 16384.0
-    accelScaleModifier4G = 8192.0
-    accelScaleModifier8G = 4096.0
-    accelScaleModifier16G = 2048.0
-    gyroScaleModifier250Deg = 131.0
-    gyroScaleModifier500Deg = 65.5
-    gyroScaleModifier1000Deg = 32.8
-    gyroScaleModifier2000Deg = 16.4
+    ACCEL_SCALE_MODIFIER_2G = 16384.0
+    ACCEL_SCALE_MODIFIER_4G = 8192.0
+    ACCEL_SCALE_MODIFIER_8G = 4096.0
+    ACCEL_SCALE_MODIFIER_16G = 2048.0
+
+    GYRO_SCALE_MODIFIER_250DEG = 131.0
+    GYRO_SCALE_MODIFIER_500DEG = 65.5
+    GYRO_SCALE_MODIFIER_1000DEG = 32.8
+    GYRO_SCALE_MODIFIER_2000DEG = 16.4
 
     # Pre-defined ranges
-    accelRange2G = 0x00
-    accelRange4G = 0x08
-    accelRange8G = 0x10
-    accelRange16G = 0x18
+    ACCEL_RANGE_2G = 0x00
+    ACCEL_RANGE_4G = 0x08
+    ACCEL_RANGE_8G = 0x10
+    ACCEL_RANGE_16G = 0x18
 
-    gyroRange250Deg = 0x00
-    gyroRange500Deg = 0x08
-    gyroRange1000Deg = 0x10
-    gyroRange2000Deg = 0x18
+    GYRO_RANGE_250DEG = 0x00
+    GYRO_RANGE_500DEG = 0x08
+    GYRO_RANGE_1000DEG = 0x10
+    GYRO_RANGE_2000DEG = 0x18
 
     # MPU-6050 Registers
     PWR_MGMT_1 = 0x6B
@@ -65,12 +66,11 @@ class MPU6050:
 
     # I2C communication methods
 
-    def ReadI2CWord(self, register):
+    def read_i2c_word(self, register):
         # Read the data from the registers
         high = self.bus.read_byte_data(self.address, register)
         low = self.bus.read_byte_data(self.address, register + 1)
 
-        # Bit magic
         value = (high << 8) + low
 
         if (value >= 0x8000):
@@ -81,9 +81,9 @@ class MPU6050:
     # MPU-6050 Methods
 
     # Returns the temperature in degrees celcius read from the temperature sensor in the MPU-6050
-    def GetTemp(self):
+    def get_temp(self):
         # Get the raw data
-        rawTemp = self.ReadI2CWord(self.TEMP_OUT0)
+        rawTemp = self.read_i2c_word(self.TEMP_OUT0)
 
         # Get the actual temperature using the formule given in the
         # MPU-6050 Register Map and Descriptions revision 4.2, page 30
@@ -93,7 +93,7 @@ class MPU6050:
         return actualTemp
 
     # Sets the range of the accelerometer to range
-    def SetAccelRange(self, accelRange):
+    def set_accel_range(self, accelRange):
         # First change it to 0x00 to make sure we write the correct value later
         self.bus.write_byte_data(self.address, self.ACCEL_CONFIG, 0x00)
 
@@ -103,20 +103,20 @@ class MPU6050:
     # Reads the range the accelerometer is set to
     # If raw is True, it will return the raw value from the ACCEL_CONFIG register
     # If raw is False, it will return an integer: -1, 2, 4, 8 or 16. When it returns -1 something went wrong.
-    def ReadAccelRange(self, raw = False):
+    def read_accel_range(self, raw = False):
         # Get the raw value
         rawData = self.bus.read_byte_data(self.address, self.ACCEL_CONFIG)
 
         if raw is True:
             return rawData
         elif raw is False:
-            if rawData == self.accelRange2G:
+            if rawData == self.ACCEL_RANGE_2G:
                 return 2
-            elif rawData == self.accelRange4G:
+            elif rawData == self.ACCEL_RANGE_4G:
                 return 4
-            elif rawData == self.accelRange8G:
+            elif rawData == self.ACCEL_RANGE_8G:
                 return 8
-            elif rawData == self.accelRange16G:
+            elif rawData == self.ACCEL_RANGE_16G:
                 return 16
             else:
                 return -1
@@ -124,26 +124,26 @@ class MPU6050:
     # Gets and returns the X, Y and Z values from the accelerometer
     # If g is True, it will return the data in g
     # If g is False, it will return the data in m/s^2
-    def GetAccelData(self, g = False):
+    def get_accel_data(self, g = False):
         # Read the data from the MPU-6050
-        x = self.ReadI2CWord(self.ACCEL_XOUT0)
-        y = self.ReadI2CWord(self.ACCEL_YOUT0)
-        z = self.ReadI2CWord(self.ACCEL_ZOUT0)
+        x = self.read_i2c_word(self.ACCEL_XOUT0)
+        y = self.read_i2c_word(self.ACCEL_YOUT0)
+        z = self.read_i2c_word(self.ACCEL_ZOUT0)
 
         accelScaleModifier = None
-        accelRange = self.ReadAccelRange(True)
+        accelRange = self.read_accel_range(True)
 
-        if accelRange == self.accelRange2G:
-            accelScaleModifier = self.accelScaleModifier2G
-        elif accelRange == self.accelRange4G:
-            accelScaleModifier = self.accelScaleModifier4G
-        elif accelRange == self.accelRange8G:
-            accelScaleModifier = self.accelScaleModifier8G
-        elif accelRange == self.accelRange16G:
-            accelScaleModifier = self.accelScaleModifier16G
+        if accelRange == self.ACCEL_RANGE_2G:
+            accelScaleModifier = self.ACCEL_SCALE_MODIFIER_2G
+        elif accelRange == self.ACCEL_RANGE_4G:
+            accelScaleModifier = self.ACCEL_SCALE_MODIFIER_4G
+        elif accelRange == self.ACCEL_RANGE_8G:
+            accelScaleModifier = self.ACCEL_SCALE_MODIFIER_8G
+        elif accelRange == self.ACCEL_RANGE_16G:
+            accelScaleModifier = self.ACCEL_SCALE_MODIFIER_16G
         else:
-            print("Unkown range - accelScaleModifier set to self.accelScaleModifier2G")
-            accelScaleModifier = self.accelScaleModifier2G
+            print("Unkown range - accelScaleModifier set to self.ACCEL_SCALE_MODIFIER_2G")
+            accelScaleModifier = self.ACCEL_SCALE_MODIFIER_2G
         
         x = x / accelScaleModifier
         y = y / accelScaleModifier
@@ -152,14 +152,14 @@ class MPU6050:
         if g is True:
             return {'x': x, 'y': y, 'z': z}
         elif g is False:
-            x = x * self.gravityMS2
-            y = y * self.gravityMS2
-            z = z * self.gravityMS2
+            x = x * self.GRAVITIY_MS2
+            y = y * self.GRAVITIY_MS2
+            z = z * self.GRAVITIY_MS2
             return {'x': x, 'y': y, 'z': z}
         
 
     # Sets the range of the gyroscope to range
-    def SetGyroRange(self, gyroRange):
+    def set_gyro_range(self, gyroRange):
         # First change it to 0x00 to make sure we write the correct value later
         self.bus.write_byte_data(self.address, self.GYRO_CONFIG, 0x00)
 
@@ -169,45 +169,45 @@ class MPU6050:
     # Reads the range the gyroscope is set to
     # If raw is True, it will return the raw value from the GYRO_CONFIG register
     # If raw is False, it will return 250, 500, 1000, 2000 or -1. If the returned value is equal to -1 something went wrong.
-    def ReadGyroRange(self, raw = False):
+    def read_gyro_range(self, raw = False):
         # Get the raw value
         rawData = self.bus.read_byte_data(self.address, self.GYRO_CONFIG)
 
         if raw is True:
             return rawData
         elif raw is False:
-            if rawData == self.gyroRange250Deg:
+            if rawData == self.GYRO_RANGE_250DEG:
                 return 250
-            elif rawData == self.gyroRange500Deg:
+            elif rawData == self.GYRO_RANGE_500DEG:
                 return 500
-            elif rawData == self.gyroRange1000Deg:
+            elif rawData == self.GYRO_RANGE_1000DEG:
                 return 1000
-            elif rawData == self.gyroRange2000Deg:
+            elif rawData == self.GYRO_RANGE_2000DEG:
                 return 2000
             else:
                 return -1
 
     # Gets and returns the X, Y and Z values from the gyroscope
-    def GetGyroData(self):
+    def get_gyro_data(self):
         # Read the raw data from the MPU-6050
-        x = self.ReadI2CWord(self.GYRO_XOUT0)
-        y = self.ReadI2CWord(self.GYRO_YOUT0)
-        z = self.ReadI2CWord(self.GYRO_ZOUT0)
+        x = self.read_i2c_word(self.GYRO_XOUT0)
+        y = self.read_i2c_word(self.GYRO_YOUT0)
+        z = self.read_i2c_word(self.GYRO_ZOUT0)
 
         gyroScaleModifier = None
-        gyroRange = self.ReadGyroRange(True)
+        gyroRange = self.read_gyro_range(True)
         
-        if gyroRange == self.gyroRange250Deg:
-            gyroScaleModifier = self.gyroScaleModifier250Deg
-        elif gyroRange == self.gyroRange500Deg:
-            gyroScaleModifier = self.gyroScaleModifier500Deg
-        elif gyroRange == self.gyroRange1000Deg:
-            gyroScaleModifier = self.gyroScaleModifier1000Deg
-        elif gyroRange == self.gyroRange2000Deg:
-            gyroScaleModifier = self.gyroScaleModifier2000Deg
+        if gyroRange == self.GYRO_RANGE_250DEG:
+            gyroScaleModifier = self.GYRO_SCALE_MODIFIER_250DEG
+        elif gyroRange == self.GYRO_RANGE_500DEG:
+            gyroScaleModifier = self.GYRO_SCALE_MODIFIER_500DEG
+        elif gyroRange == self.GYRO_RANGE_1000DEG:
+            gyroScaleModifier = self.GYRO_SCALE_MODIFIER_1000DEG
+        elif gyroRange == self.GYRO_RANGE_2000DEG:
+            gyroScaleModifier = self.GYRO_SCALE_MODIFIER_2000DEG
         else:
-            print("Unkown range - gyroScaleModifier set to self.gyroScaleModifier250Deg")
-            gyroScaleModifier = self.gyroScaleModifier250Deg
+            print("Unkown range - gyroScaleModifier set to self.GYRO_SCALE_MODIFIER_250DEG")
+            gyroScaleModifier = self.GYRO_SCALE_MODIFIER_250DEG
         
         x = x / gyroScaleModifier
         y = y / gyroScaleModifier
@@ -216,21 +216,21 @@ class MPU6050:
         return {'x': x, 'y': y, 'z': z}      
 
     # Gets and returns the X, Y and Z values from the accelerometer and from the gyroscope and the temperature from the temperature sensor
-    def GetAllData(self):
-        temp = GetTemp()
-        accel = GetAccelData()
-        gyro = GetGyroData()
+    def get_all_data(self):
+        temp = get_temp()
+        accel = get_accel_data()
+        gyro = get_gyro_data()
 
         return [accel, gyro, temp]
 
 if __name__ == "__main__":
     mpu = MPU6050(0x68)
-    print(mpu.GetTemp())
-    accelData = mpu.GetAccelData()
+    print(mpu.get_temp())
+    accelData = mpu.get_accel_data()
     print(accelData['x'])
     print(accelData['y'])
     print(accelData['z'])
-    gyroData = mpu.GetGyroData()
+    gyroData = mpu.get_gyro_data()
     print(gyroData['x'])
     print(gyroData['y'])
     print(gyroData['z'])
